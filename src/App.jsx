c00e46a27cfa31273323953b6ef1b2a69c4b308c
mdiktushar <md.tushar1804@gmail.com>
2023-04-14 18:00:30 +0600

    google signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "./firebase/firebase.config";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const App = () => {
+  const [user, setUser] = useState(null);
+  const googleSignInHangler = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+      console.log(result.user);
+        setUser(result.user)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Firebase + React</h1>
+      <button onClick={googleSignInHangler}>Google Sign In</button>
+      {
+        user && <div>
+          <h5>User: {user.displayName}</h5>
+        </div>
+      }
+    
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLUTcWF3nwmBdYDlT_dIni9pHjDvJhdDk",
+  authDomain: "auth-practice-8d897.firebaseapp.com",
+  projectId: "auth-practice-8d897",
+  storageBucket: "auth-practice-8d897.appspot.com",
+  messagingSenderId: "565287006947",
+  appId: "1:565287006947:web:554629b79e2b8183a7cbd6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
